e13e38d01eafbbd0800f84138525f87c79e8cbfe
Daniela Pedraza <pedrazao@unlv.nevada.edu>
2024-12-05 22:50:13 -0800

    Added missing library doc pages

---
+++ src/app/docs/libraries/motion-sensor/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Content from "@/components/content/content";
+import NavWrapper from "@/components/navigation/nav-wrapper";
+import UnderDevelopment from "@/components/placeholder/under-development";
+import { useRefMap } from "@/hooks/useRefMap";
+import { ContentHeaderType } from "@/types/content-header";
+
+// ! update page name
+export default function MotionSensorPage() {
+  // generate ref map using these names
+  const refMap = useRefMap([]);
+
+  // define content headers
+  const headers: ContentHeaderType[] = [];
+
+  return (
+    <NavWrapper title="MotionSensor" description="Under development" headers={headers}>
+      <Content>
+        <UnderDevelopment />
+      </Content>
+    </NavWrapper>
+  );
+}
